0431ae564fa500b2ff2f0fedb143455631da9144
Rhea Shetti <shettir@uci.edu>
2025-05-02 11:46:24 -0700

    fixing files

---
+++ phsyics20b/src/pages/conclusion.tsx
@@ -0,0 +1,14 @@
+const Conclusion: React.FC = () => {
+
+    return (
+      <section id="conclusion">
+          <h2> The Push for Regulation </h2>
+          <h4> In an effort to address these concerns, the American Astronomical Society hosted SATCON1 and SATCON2, workshops focused on discussing the growing impact of mega constellations. SATCON1 explored the negative effects on ground-based astronomy and proposed strategies such as darkening and reorienting satellites to reduce their interference with observations. It also emphasized the need for international cooperation in the matter and concluded with SpaceX agreeing to experiment with darker coatings and reorientation to reduce the brightness of their satellites. </h4>
+          <h4> SATCON2 was called to further address the issue of light pollution. In addition to technical discussions, it was decided that public outreach was also needed to raise public awareness of the potential consequences of these mega constellations. The workshop’s conclusion called for the establishment of a central coordinating body to oversee mitigation efforts and other tools that would help astronomers plan around passing Starlink satellites. </h4>
+          <h4> Though these conferences were an important step in developing regulations to lower the risk of mega constellations, no official regulations were established. Additionally, the conferences only mainly focused on the astronomical impact, leaving the enviornmental consequences still unaddressed. </h4>
+      </section>
+      
+    );
+  };
+  
+  export default Conclusion;
